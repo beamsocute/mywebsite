1022e488ba5413fa29868e81fdda8e0ea35c1eb7
beamsocute <98265302+beamsocute@users.noreply.github.com>
2022-02-11 01:05:18 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,57 @@
+<html>
+        <head>
+                <title>งานที่ 3</title>
+        </head>
+        <body bgcolor="F75353">
+                <table border="1">
+                        <tr>
+                                <th>ลำดับ</th>
+                                <th>ชื่อ</th>
+                                <th>ชื่อเล่น</th>
+                                <th>รูป</th>
+                                <th>twitter</th>
+                        </tr>
+                        <tr>
+                                <td>1</td>
+                                <td>MILIN DOKTHIAN</td>
+                                <td>Namneung</td>
+                                <td><img src="https://twitter.com/marnpdt/status/1491643701629976578?s=20&t=6bvYU0goWHKNBy5ehOimhQ" width="130px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>2</td>
+                                <td>Jung Ho-seok</td>
+                                <td>Jhope</td>
+                                <td><img src="https://www.instagram.com/p/CXM7ZgzvJb5/?utm_source=ig_web_copy_link" width="140px"></td>
+                                <td><a href="https://twitter.com/bts_bighit?s=21&fbclid=IwAR36yzn8MImuJ9bZmf0-47gHshmFvAUkQWwFMmhJpqCNNLGpj5XZePAVxp8">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>3</td>
+                                <td>NATRUJA CHUTIWANSOPON</td>
+                                <td>Kaew</td>
+                                <td><img src="https://www.instagram.com/p/CY6SV29vj04/?utm_source=ig_web_copy_link" width="140px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>4</td>
+                                <td>KANTEERA WADCHARATHADSANAKUL</td>
+                                <td>Noey</td>
+                                <td><img src="https://pin.it/4QXmplm" width="130px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>5</td>
+                                <td>WEERAYA ZHANG</td>
+                                <td>Wee</td>
+                                <td><img src="https://www.instagram.com/p/CVAvajqhDuC/?utm_source=ig_web_copy_link" width="130px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>6</td>
+                                <td> PIMRAPAT PHADUNGWATANACHOK</td>
+                                <td>Mobile</td>
+                                <td><img src="https://www.instagram.com/p/CMOm2Z6JyCu/?utm_source=ig_web_copy_link" width="130px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
+        </body>
+</html>
